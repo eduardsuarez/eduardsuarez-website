26762227b2692b575645c126a6c480f15cfd6ada
Eduard Suárez <suareze205@gmail.com>
2023-04-25 13:45:28 -0500

    The Navbar component is created and I have added the logo and elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     
-    <div>
-      website
-    </div>
+    <Navbar />
   )
 }
