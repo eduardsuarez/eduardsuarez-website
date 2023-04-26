f7771a01616febdf5452e6264bf24f945b3cebf8
Eduard Suárez <suareze205@gmail.com>
2023-04-26 13:27:26 -0500

    I have used the hook useState to hide or show the nav component depending on the screen resolution

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,20 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai"
+
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai"
+import { FaGithub, FaLinkedinIn } from "react-icons/fa"
+import { BsFillPersonLinesFill } from "react-icons/bs"
+import { useState } from "react";
+
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
     return (
         <div className="fixed w-full h-20 shadow-xl z-[100]">
             <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -27,18 +39,22 @@ const Navbar = () => {
 
                     </ul>
 
-                    <div className="md:hidden">
+                    <div onClick={handleNav} className="md:hidden">
                         <AiOutlineMenu size={25} />
                     </div>
                 </div>
             </div>
 
-            <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
-                <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500 ">
+            <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+                <div className={
+                    nav
+                        ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                        : 'fixed left-[-100%] top-0 p-10 ease-in duration-500 '
+                }>
                     <div>
                         <div className="flex w-full items-center justify-between">
                             <Image src="/../public/assets/NavbarLogo.png" width="87" height="35" alt="/" />
-                            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                            <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
                                 <AiOutlineClose />
                             </div>
                         </div>
@@ -64,11 +80,28 @@ const Navbar = () => {
                                 <li className="py-4  text-sm  hover:border-b">Contact</li>
                             </Link>
                         </ul>
+                        <div className="pt-16">
+                            <p className="uppercase tracking-widest text-[#5651e5]"> Lets Connect</p>
+                            <div className="flex items-center justify-between my-4 w-full sm:w-[80%] ">
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <FaLinkedinIn />
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <FaGithub />
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <AiOutlineMail />
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <BsFillPersonLinesFill />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>    
+            </div>
         </div>
-        
+
 
 
     )
